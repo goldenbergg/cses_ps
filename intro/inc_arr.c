2e0f46a04406fadea96a04795b187e19715556c0
goldenbergg <ggoldenb@purdue.edu>
2022-02-24 00:26:30 -0500

    finished increasing array

--- intro/inc_arr.c
+++ intro/inc_arr.c
@@ -4,7 +4,30 @@
 
 int main(int argc, char **argv)
 {
-	int64_t nums;
+	char temp;
+	size_t size;
 
-	if (scanf(%
+	if (scanf("%ld", &size) == EOF) {
+		printf("scanf: EOF\n");
+	}
+
+	if (scanf("%c", &temp) == EOF) {
+		printf("scanf: EOF\n");
+	}
+
+	int32_t elem;
+	int64_t min_moves = 0;
+
+	for (size_t loop_idx = 0, curr_max = 0; loop_idx < size; loop_idx++) {
+		if (scanf("%d", &elem) == EOF) {
+			printf("scanf: EOF\n");
+		}
+
+		curr_max = ((uint32_t) elem > curr_max) ? elem : curr_max;
+		min_moves += curr_max - elem;
+	}
+	
+	printf("%ld", min_moves);
+
+	return 0;
 }
